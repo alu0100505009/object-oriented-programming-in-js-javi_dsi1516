0af1034d4841fa9c1ba2a02bf09af4e195578fc7
alu0100505009 <alu0100505009@ull.edu.es>
2016-03-06 17:33:33 +0000

    Añadido conversor de € a $

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -14,12 +14,12 @@
     };
   }
   
-  function Temperatura(valor,tipo){
+  function Conversor(valor,tipo){
     Medida.call(this, valor, tipo);
   }
   
   function Celsius(valor){
-    Temperatura.call(this, valor, "c");
+    Conversor.call(this, valor, "c");
 		this.toFarenheit = function (){
       return (this.getValor() * 9/5 + 32);
     };
@@ -29,7 +29,7 @@
   }
   
   function Farenheit(valor){
-    Temperatura.call(this, valor, "f");
+    Conversor.call(this, valor, "f");
 		this.toCelsius = function () {
       return ((this.getValor() - 32) * 5/9);
     };
@@ -39,8 +39,8 @@
     };
   }
   
-  var Kelvin = function(valor){
-    Temperatura.call(this, valor, "k");
+  function Kelvin(valor){
+    Conversor.call(this, valor, "k");
     this.toFarenheit = function () {
 		  return ((this.getValor() - 273.15) * 9/5 + 32);
     };
@@ -50,25 +50,55 @@
 		};
   }
   
+  function Metro(valor){
+    Conversor.call(this, valor, "m");
+    this.toPulgadas = function(){
+      return (this.getValor() * 39.3701);
+    };
+  }
+  
+  function Euro(valor){
+    Conversor.call(this, valor, "€");
+    this.toDollar = function(){
+      return (parseInt(this.getValor()) * 1.09995);
+    };
+  }
+  
+  function Dollar(valor){
+    Conversor.call(this, valor, "$");
+    this.toEuro = function(){
+      return (parseInt(this.getValor()) * 0.909132233);
+    };
+  }
 
-  Temperatura.prototype = new Medida();
-  Temperatura.prototype.constructor = Temperatura;
+  Conversor.prototype = new Medida();
+  Conversor.prototype.constructor = Conversor;
 
-  Celsius.prototype = new Temperatura();
+  Celsius.prototype = new Conversor();
 	Celsius.prototype.constructor = Celsius;
-	Farenheit.prototype = new Temperatura();
+	Farenheit.prototype = new Conversor();
 	Farenheit.prototype.constructor = Farenheit;
-	Kelvin.prototype = new Temperatura();
+	Kelvin.prototype = new Conversor();
 	Kelvin.prototype.constructor = Kelvin;
-
-  exports.Temperatura = Temperatura;
+	Metro.prototype = new Conversor();
+	Metro.prototype.constructor = Metro;
+	Euro.prototype = new Conversor();
+	Euro.prototype.constructor = Euro;
+	Dollar.prototype = new Conversor();
+	Dollar.prototype.constructor = Dollar;
+	
+  exports.Conversor = Conversor;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
+  exports.Kelvin = Kelvin;
+  exports.Metro = Metro;
+  exports.Euro = Euro;
+  exports.Dollar = Dollar;
 
   exports.convertir = function() {
     var valor     = document.getElementById('convert').value;
     var elemento  = document.getElementById('converted');
-    var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fkc])\s*(?:to)?\s*([fkc])$/i;
+    var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fkc€$])\s*(?:to)?\s*([fkc€$])$/i;
     valor = valor.match(regexp);
     
     if (valor) {
@@ -79,14 +109,13 @@
       numero = parseFloat(numero);
       console.log("Valor: " + numero + valorFrom + " to " + valorTo);
       
-            switch (valorFrom) {
+      switch (valorFrom) {
         case 'c':
           var celsius = new Celsius(numero);
           if (valorTo == 'f')
             elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
           else if (valorTo == 'k')
             elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin";
-          //elemento.innerHTML = "HOLA";
           break;
           
         case 'f':
@@ -103,6 +132,18 @@
             elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
           else if (valorTo == 'f')
             elemento.innerHTML = kelvin.toFarenheit().toFixed(2) + " Farenheit";
+          break;
+          
+        case '€':
+          var euro = new Euro(valor);
+          if (valorTo == '$')
+            elemento.innerHTML = euro.toDollar().toFixed(2) + " Dollars";
+          break;
+          
+        case '$':
+          var dollar = new Dollar(valor);
+          if (valorTo == '€')
+            elemento.innerHTML = dollar.toEuro().toFixed(2) + " Euros";
           break;
         
         default:
