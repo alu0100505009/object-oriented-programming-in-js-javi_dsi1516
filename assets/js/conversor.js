26dc749493bcb46e7bf7e9d0cf8b788a8c797779
alu0100505009 <alu0100505009@ull.edu.es>
2016-03-06 16:42:43 +0000

    Prueba OOP en JS

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,35 +1,75 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
-  {
-    /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
-    /* ademas de new Medida(45.2, "Km") */
+  function Medida(valor,tipo){
+    var _valor = valor;
+    var _tipo = tipo || "type not defined";
+
+    this.getValor = function(){
+      return _valor;
+    };
+
+    this.getTipo = function(){
+      return _tipo;
+    };
   }
   
-  function Temperatura(valor,tipo)
-  {
-    /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+  function Temperatura(valor,tipo){
+    Medida.call(this, valor, tipo);
   }
   
-  function Celsius(valor)
-  {
+  function Celsius(valor){
+    Temperatura.call(this, valor, "c");
+		this.toFarenheit = function (){
+      return (this.getValor() * 9/5 + 32);
+    };
+		this.toKelvin = function (){
+      return (parseInt(this.getValor()) + 273.15);
+    };
   }
   
-  function Farenheit(valor)
-  {
+  function Farenheit(valor){
+    Temperatura.call(this, valor, "f");
+		this.toCelsius = function () {
+      return ((this.getValor() - 32) * 5/9);
+    };
+
+    this.toKelvin = function () {
+      return ((this.getValor() - 32) * 5/9 + 273.15);
+    };
   }
   
+  var Kelvin = function(valor){
+    Temperatura.call(this, valor, "k");
+    this.toFarenheit = function () {
+		  return ((this.getValor() - 273.15) * 9/5 + 32);
+    };
+
+    this.toCelsius = function () {
+		  return (this.getValor() - 237.15);
+		};
+  }
+  
+
+  Temperatura.prototype = new Medida();
+  Temperatura.prototype.constructor = Temperatura;
+
+  Celsius.prototype = new Temperatura();
+	Celsius.prototype.constructor = Celsius;
+	Farenheit.prototype = new Temperatura();
+	Farenheit.prototype.constructor = Farenheit;
+	Kelvin.prototype = new Temperatura();
+	Kelvin.prototype.constructor = Kelvin;
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
 
   exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
-    valor     = valor.match(regexp);
+    var valor     = document.getElementById('convert').value;
+    var elemento  = document.getElementById('converted');
+    var regexp = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fkc])\s*(?:to)?\s*([fkc])$/i;
+    valor = valor.match(regexp);
     
     if (valor) {
       var numero = valor[1],
@@ -49,7 +89,7 @@
           break;
         
         default:
-          /* rellene este código */
+           elemento.innerHTML = "Tipo no reconocido";
       }
     }
     else
